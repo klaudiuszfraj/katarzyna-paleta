1ac0e8de65db2b44c64367717f756560c45b528d
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2021-05-03 17:46:17 +0200

    set basic meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Katarzyna Paleta - malarstwo, sztukaterie, galeria obrazów, portret, akt',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'malarstwo, portrety,sztukaterie,galeria obrazów, portret,akt',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
